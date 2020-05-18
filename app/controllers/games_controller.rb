17a0765f5f2503ae8a1a9475b9a9c66735234e97
Daniela Gordas <daniela.gordas@gmail.com>
2020-05-18 16:47:45 +0200

    added new and score actions and implementend selection of 10 random letters

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,10 @@
+class GamesController < ApplicationController
+  def new
+    # used to display a new random grid and a form
+    @letters = ('A'..'Z').to_a.sample(10).join(' ')
+  end
+
+  def score
+    # The form will be submitted (with POST) to the score action.
+  end
+end
